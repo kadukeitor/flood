7f8e018f3ad6bf525c0c2f1fecc40e062a773d8f
Jorge Garcia <kadukeitor@gmail.com>
2019-03-07 16:00:56 -0500

    High Score

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,16 @@ import 'dart:io';
 import 'dart:math';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => FutureBuilder(
+  Widget build(ctx) => FutureBuilder(
       future: SystemChrome.setPreferredOrientations(
           [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]),
-      builder: (BuildContext context, _) =>
+      builder: (ctx, _) =>
           MaterialApp(home: Game(), debugShowCheckedModeBanner: false));
 }
 
@@ -20,7 +21,7 @@ class Game extends StatefulWidget {
 }
 
 class _Game extends State<Game> {
-  int size = 14, max = 30, moves = 0;
+  int size = 14, max = 30, moves = 0, best = 0;
   List<Color> colors = [
     Colors.red,
     Colors.blue,
@@ -31,7 +32,7 @@ class _Game extends State<Game> {
   ];
   List<List<int>> board = [];
 
-  generate() {
+  gen() {
     List<List<int>> _board = [];
     Random random = new Random.secure();
     for (int i = 0; i < size; i++) {
@@ -45,57 +46,67 @@ class _Game extends State<Game> {
     });
   }
 
-  select(int color) async {
-    if (board[0][0] == color || completed() || loosed()) return false;
+  sel(int color) {
+    if (board[0][0] == color || win() || lose()) return false;
     setState(() {
       moves += 1;
     });
-    paint(0, 0, color);
-    if (completed())
-      dialog(Column(mainAxisSize: MainAxisSize.min, children: [
+    pnt(0, 0, color);
+    if (win())
+      dlg(Column(mainAxisSize: MainAxisSize.min, children: [
         Icon(Icons.sentiment_satisfied, size: 48),
-        text("YOU WIN")
+        txt("YOU WIN")
       ]));
-    if (loosed())
-      dialog(Column(mainAxisSize: MainAxisSize.min, children: [
+    else if (lose())
+      dlg(Column(mainAxisSize: MainAxisSize.min, children: [
         Icon(Icons.sentiment_dissatisfied, size: 48),
-        text("YOU LOOSE")
+        txt("YOU LOSE")
       ]));
   }
 
-  paint(int c, int r, int color) {
+  pnt(int c, int r, int color) {
     int oldColor = board[c][r];
     setState(() {
       board[c][r] = color;
     });
-    if (c - 1 >= 0 && board[c - 1][r] == oldColor) paint(c - 1, r, color);
-    if (r + 1 < size && board[c][r + 1] == oldColor) paint(c, r + 1, color);
-    if (c + 1 < size && board[c + 1][r] == oldColor) paint(c + 1, r, color);
-    if (r - 1 >= 0 && board[c][r - 1] == oldColor) paint(c, r - 1, color);
+    if (c - 1 >= 0 && board[c - 1][r] == oldColor) pnt(c - 1, r, color);
+    if (r + 1 < size && board[c][r + 1] == oldColor) pnt(c, r + 1, color);
+    if (c + 1 < size && board[c + 1][r] == oldColor) pnt(c + 1, r, color);
+    if (r - 1 >= 0 && board[c][r - 1] == oldColor) pnt(c, r - 1, color);
   }
 
-  loosed() => moves >= max;
+  lose() => moves >= max;
 
-  completed() {
+  win() {
     int color = board[0][0];
     for (int i = 0; i < size; i++)
       for (int j = 0; j < size; j++) if (color != board[i][j]) return false;
+    if (best == null || moves < best) {
+      setState(() => best = moves);
+      SharedPreferences.getInstance()
+          .then((prefs) => prefs.setInt('best', moves));
+    }
     return true;
   }
 
-  text(s, [f = 14.0]) => Text(s, style: TextStyle(fontSize: f));
+  pad(c, [p = 8.0]) => Padding(padding: EdgeInsets.all(p), child: c);
 
-  dialog(w) =>
+  txt(s, [f = 14.0]) =>
+      Text(s, style: TextStyle(fontSize: f, color: Colors.black));
+
+  dlg(w) =>
       showDialog(context: context, builder: (_) => AlertDialog(content: w));
 
   @override
   void initState() {
     super.initState();
-    generate();
+    gen();
+    SharedPreferences.getInstance()
+        .then((prefs) => setState(() => best = prefs.get('best')));
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext ctx) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -105,38 +116,37 @@ class _Game extends State<Game> {
         actions: <Widget>[
           IconButton(
               icon: Icon(Icons.help),
-              onPressed: () => dialog(
+              onPressed: () => dlg(
                   Image.asset('assets/images/help.png', fit: BoxFit.contain))),
-          IconButton(icon: Icon(Icons.refresh), onPressed: () => generate())
+          IconButton(icon: Icon(Icons.refresh), onPressed: () => gen())
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
+      body: pad(
+        Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 16),
-              child: Column(
-                children: <Widget>[
-                  text('MOVES', 18.0),
-                  text(moves.toString() + '/' + max.toString(), 28.0)
-                ],
-              ),
+          children: [
+            Column(
+              children: [
+                txt('MOVES', 18.0),
+                txt(moves.toString() + '/' + max.toString(), 28.0)
+              ],
             ),
             Flexible(
-              child: GridView.count(
-                  primary: false,
-                  mainAxisSpacing: 2,
-                  crossAxisSpacing: 2,
-                  crossAxisCount: size,
-                  children: board
-                      .expand((pair) => pair)
-                      .toList()
-                      .map((int v) =>
-                          GridTile(child: Container(color: colors[v])))
-                      .toList()),
-            ),
+                child: pad(GridView.count(
+                    primary: false,
+                    mainAxisSpacing: 2,
+                    crossAxisSpacing: 2,
+                    crossAxisCount: size,
+                    children: board
+                        .expand((pair) => pair)
+                        .toList()
+                        .map((int v) =>
+                            GridTile(child: Container(color: colors[v])))
+                        .toList()))),
+            pad(Column(children: [
+              txt('HIGH SCORE', 16.0),
+              txt((best ?? '-').toString(), 20.0)
+            ])),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: colors
@@ -146,7 +156,7 @@ class _Game extends State<Game> {
                           elevation: 0,
                           color: color,
                           shape: CircleBorder(),
-                          onPressed: () => select(
+                          onPressed: () => sel(
                               colors.indexWhere((_color) => _color == color)))))
                   .toList(),
             )
